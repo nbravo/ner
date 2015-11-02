61bcdba6d646592db81f6d894b2bd286ad275380
nbravo <nbravo@mit.edu>
2015-11-02 14:21:16 -0500

    first commit. Non-contextual works

---
+++ maxent_model.py
@@ -0,0 +1,165 @@
+import numpy as np
+import scipy
+import sys
+from sklearn import linear_model
+from collections import defaultdict
+import preprocess
+import gensim
+from sklearn.externals import joblib
+
+tags = {"UNKNOWN": 0, "O": 1, "B": 2, "I": 3}
+reverse_tags = {0: "UNKNOWN", 1: "O", 2: "B", 3: "I"}
+word_dict = None
+numFeatures = None
+
+def translate(predictions):
+  new_predictions = []
+  prev_tag = None
+  for prediction in predictions:
+    if prediction == 0 or prediction == 1:
+      new_predictions.append("TAG")
+    if prediction == 3:
+      if prev_tag == "GENE1" or prev_tag == "GENE2":
+        new_predictions.append(prev_tag)
+      else:
+        new_predictions.append("GENE1")
+        prev_tag = "GENE1"
+    if prediction == 2:
+      if prev_tag == "GENE1":
+        new_predictions.append("GENE2")
+        prev_tag = "GENE2"
+      else:
+        new_predictions.append("GENE1")
+        prev_tag = "GENE1"
+  return new_predictions
+
+if __name__ == "__main__":
+  train = sys.argv[1]
+  test = sys.argv[2]
+  model_number = sys.argv[3]
+  X = None
+  y = None
+
+  retagged_train, word_dict = preprocess.iob(train)
+  global numFeatures
+  numWords = len(word_dict)
+  numFeatures = numWords + 3 + 4
+  X = scipy.sparse.lil_matrix((numWords, numFeatures))
+  y = np.empty([numWords, ])
+  training_seen = [0] * numWords
+  with open(retagged_train) as f:
+    identifier_line = True
+    split_line = None
+    word = None
+    tag = None
+    length = None
+    alpha = None
+    allCaps = None
+    hasNumeric = None
+    counts = None
+    index = None
+    oneHot = None
+    rowIndex = 0
+    yIndex = 0
+    for line in f:
+      if not identifier_line:
+        split_line = line.rstrip('\n').split(" ")
+        for token in split_line:
+          word, tag = token.split("_")
+
+          counts, index = word_dict[word.lower()]
+          if training_seen[index] == 0:
+            length = len(word)
+            alpha = 1 if word.isalpha() else 0
+            allCaps = 1 if word.upper() == word else 0
+            hasNumeric = 1 if any(c.isdigit() for c in word) else 0
+
+            oneHot = [0] * len(word_dict)
+            oneHot[index] = 1
+            X[rowIndex] = np.array(oneHot + counts + [length, alpha, allCaps, hasNumeric])
+            rowIndex += 1
+            training_seen[index] = 1
+
+            y[yIndex] = np.array(tags[tag])
+            yIndex += 1
+      identifier_line = not identifier_line
+
+  X = X.asformat("csr")
+
+
+  model = linear_model.LogisticRegression()
+  model.fit(X, y)
+  joblib.dump(model, 'model1.pkl')
+
+  testWords = preprocess.getWordDict(test)
+  X = scipy.sparse.lil_matrix((len(testWords), numFeatures))
+  length = None
+  alpha = None
+  allCaps = None
+  hasNumeric = None
+  counts = None
+  index = None
+  oneHot = None
+  words = None
+  x = None
+  predictions = None
+  new_line = None
+  rowIndex = 0
+  wordIndex = 0
+  for word in testWords:
+    testWords[word] = wordIndex
+    wordIndex += 1
+    length = len(word)
+    alpha = 1 if word.isalpha() else 0
+    allCaps = 1 if word.upper() == word else 0
+    hasNumeric = 1 if any(c.isdigit() for c in word) else 0
+
+    counts = [0, 0, 0]
+    index = -1
+    oneHot = [0] * len(word_dict)
+    if word.lower() in word_dict:
+      counts, index = word_dict[word.lower()]
+    if index >= 0:
+      oneHot[index] = 1
+    X[rowIndex] = np.array(oneHot + counts + [length, alpha, allCaps, hasNumeric])
+    rowIndex += 1
+  X = X.asformat("csr")
+  predictions = model.predict(X)
+
+  split_line = None
+  word = None
+  tag = None
+  new_line = []
+  newPred = None
+  prev_tag = None
+  with open(test) as f:
+    identifier_line = True
+    for line in f:
+      if identifier_line:
+        print line.rstrip('\n')
+      else:
+        split_line = line.split(" ")
+        new_line = []
+        prev_tag = None
+        for token in split_line:
+          word, tag = token.rstrip('\n').split("_")
+          prediction = predictions[testWords[word]]
+          if prediction == 0 or prediction == 1:
+            newPred = "TAG"
+          if prediction == 3:
+            if prev_tag == "GENE1" or prev_tag == "GENE2":
+              newPred = prev_tag
+            else:
+              newPred = "GENE1"
+              prev_tag = "GENE1"
+          if prediction == 2:
+            if prev_tag == "GENE1":
+              newPred = "GENE2"
+              prev_tag = "GENE2"
+            else:
+              newPred = "GENE1"
+              prev_tag = "GENE1"
+          new_line.append(word + "_" + newPred)
+        print ' '.join(new_line)
+      identifier_line = not identifier_line
+
---
+++ preprocess.py
@@ -0,0 +1,74 @@
+import sys
+
+word_dict = {}
+uniqueIndex = 0
+''' Returns fileName of the retagged file along with a dictionary.
+word: [NumSeenO, NumSeenB, NumSeenI]
+'''
+def iob(fileName):
+  outfileContents = []
+  with open(fileName) as f:
+    identifier_line = True
+    for line in f:
+      if identifier_line:
+        outfileContents.append(line)
+      else:
+        outfileContents.append(retag(line))
+      identifier_line = not identifier_line
+  name, ext = fileName.split(".")
+  outfileName = name + "_retagged." + ext
+  g = open(outfileName, 'w')
+  g.write(''.join(outfileContents))
+  g.close()
+  return outfileName, word_dict
+
+def getWordDict(fileName):
+  scopedWordDict = {}
+  with open(fileName) as f:
+    identifier_line = True
+    for line in f:
+      if not identifier_line:
+        split_line = line.rstrip('\n').split(" ")
+        for token in split_line:
+          word,tag = token.split("_")
+          scopedWordDict[word] = 0
+      identifier_line = not identifier_line
+  return scopedWordDict
+
+'''
+Input: a_GENE1 b_GENE1 c_TAG d_GENE2
+Output: a_B b_I c_O d_B
+'''
+def retag(line):
+  global uniqueIndex
+  new_tokens = []
+  prev_tag = None
+  split_line = line.rstrip('\n').split(" ")
+  for token in split_line:
+
+    word,tag = token.split("_")
+    counts = None
+    currIndex = None
+    if word.lower() in word_dict:
+      counts, currIndex = word_dict[word.lower()]
+    else:
+      counts = [0, 0, 0]
+      currIndex = uniqueIndex
+      uniqueIndex += 1
+    if tag == "TAG":
+      new_tokens.append(word + "_O")
+      counts[0] = counts[0] + 1
+    else:
+      if tag == prev_tag:
+        new_tokens.append(word + "_I")
+        counts[2] = counts[2] + 1
+      else:
+        new_tokens.append(word + "_B")
+        counts[1] = counts[1] + 1
+    word_dict[word.lower()] = counts, currIndex
+    prev_tag = tag
+  return ' '.join(new_tokens) + '\n'
+
+
+if __name__ == "__main__":
+  iob(sys.argv[1])
